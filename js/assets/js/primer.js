9a3f4fef876eea9b66a40b87dcacc4b4d4c02b3b
edward <edward_p14@hotmail.com>
2021-09-22 20:20:33 -0500

    clase JavaScript

---
+++ js/assets/js/primer.js
@@ -0,0 +1,18 @@
+console.log("Hola Mundo"); //System.out.print()   
+var data1 = "hola";
+console.log(data1);
+let data2 = 2.45;
+console.log(data2);
+var data3 = false;
+let data4 = 'hola a todos';
+console.log(data4)
+const PI = 3.1415;
+let result = data2 + PI;
+console.log(result);
+//alert(result);
+if(PI > 2){
+    alert("soy mayor" + " " + PI);
+}
+else{
+    console.log("hola");
+}
